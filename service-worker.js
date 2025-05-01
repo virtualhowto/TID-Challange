f5f5a207e4c666740d06c308bd764961a852b816
Scott McGregor <scottm@hyperscott.com>
2025-05-02 00:10:25 +1000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@ self.addEventListener('install', event => {
     caches.open('tid-challenge-cache').then(cache => {
       return cache.addAll([
         './index.html','./style.css','./script.js','./data.json','./manifest.json',
-        './correct.mp3','./wrong.mp3','./icon-192.png','./icon-512.png','./mdcnsw.png'
+        './correct.mp3','./wrong.mp3','./icon-192.png','./icon-512.png','./tid-logo.png'
       ]);
     })
   );
